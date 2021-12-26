bbec93a01f08c094bdc5c64616ce36638b8a908e
pedroddvo <pedroddvo@gmail.com>
2021-12-26 14:45:06 +0000

    Start of hand-rolled parser

--- src/lang.rs
+++ src/lang.rs
@@ -1,3 +1,1 @@
-pub mod error;
-pub mod parse;
-pub mod interpreter;
+pub mod tokenize;
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,6 @@
-extern crate nom;
-extern crate nom_locate;
 mod lang;
 
 
 fn main() {
   let src = std::fs::read_to_string("example.sfd").unwrap();
-  match lang::parse::parse(src.as_str()) {
-    Ok(o) => {
-      println!("Success:\n{:?}\n{:#?}", o.0, o.1);
-      
-      let mut e = lang::interpreter::Env {};
-      println!("Interpreted top: {:?}", e.expr(o.1));
-    }
-    Err(nom::Err::Error(e)) => {
-      println!("{}", lang::error::report_error(src.as_str(), e));
-    }
-    _ => {}
-  }
 }
